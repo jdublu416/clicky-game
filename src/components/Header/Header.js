8d47e0727cf2aa34e8907a4e02f86cae8aabdca2
Jdublu <jewljbl13@aol.com>
2018-06-12 20:27:10 -0400

    adding more component code and picture.json file and image folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Clicky Game!</h1>
+    <h2>
+      Click on an image to earn one point! Click on any more than once, GAME OVER!
+    </h2>
+  </header>
+);
+
+export default Header;
+
