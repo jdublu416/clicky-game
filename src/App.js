abebff0c98c72049cef42180031eb91c6123dfe1
Jdublu <jewljbl13@aol.com>
2018-06-18 10:51:36 -0400

    update to methods in app.js

--- src/App.js
+++ src/App.js
@@ -10,29 +10,36 @@ import PictCard from "./components/PictCard";
 class App extends Component {
   state = {
     images,
-    count: 0
+    gamePlay,
+    count: 0,
+    topScore:0
   };
+  
 
   handleClickOfImages = id => {
-    const images = this.state.images.filter(
-      image => (this.image.id = { clicked: true })
-    );
-    this.setState({ images});
+    const images = this.state.images.filter(image =>  image.id === id);
+    this.setState({ images });
+    // this.setState({topScore})
+    this.handleGamePlay(this.id);
   };
+ 
   handleGamePlay = id => {
-    if (this.clicked) {
-      //end game
-    }
-    else{
-      //Increase score
+    if (!this.clicked) {
+      this.clicked = true;
       this.setState({ count: this.state.count + 1 });
-    }
-   // checkForEndGame => {
-      //conditional statement that checks the clicked status when user clicks an image and if it is true then shake 
-      //changes to true, alert to user game is over.reset count to zero and check for high score.
+    } else {
+      if (this.state.count > this.state.topScore) {
+        this.topScore = this.setState({ topScore: this.state.count});
+        this.setState({ count: 0 });
+      } else {
+        this.setState({ count: 0 });
+      };
+    // checkForEndGame => {
+    //conditional statement that checks the clicked status when user clicks an image and if it is true then shake
+    //changes to true, alert to user game is over.reset count to zero and check for high score.
     //}
-  };
-
+  }
+}
   //   handleGameOver = clicked => {
   //     const gameOver = this.state.images.filter(image => image.clicked===true);
   //   }
